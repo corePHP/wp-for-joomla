3ba2689aef25ecef0e96bd7c6407856308bc8215
Steven Pignataro <steven@corephp.com>
2013-09-26 22:11:56 -0400

    Header and footer fix
    - still need to figure out how to get the main stuff in the header of the template.
    
    Signed-off-by: Steven Pignataro <steven@corephp.com>

--- wp30/src/components/com_wordpress/wp/wp-content/themes/twentytwelve/footer.php
+++ wp30/src/components/com_wordpress/wp/wp-content/themes/twentytwelve/footer.php
@@ -13,12 +13,20 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
-			<?php do_action( 'twentytwelve_credits' ); ?>
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
+			<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+				<?php bloginfo( 'name' ); ?>
+			</a>
 		</div><!-- .site-info -->
+
+		<div id="site-generator">
+			<?php do_action( 'twentytwelve_credits' ); ?>
+			Blog powered by <a href="<?php echo esc_url( __('http://wordpress.org/', 'twentyten') ); ?>"
+					title="<?php esc_attr_e('Semantic Personal Publishing Platform', 'twentyten'); ?>" rel="generator">
+				<?php printf( __('%s.', 'twentyten'), 'WordPress' ); ?>
+			</a><br />
+			<a href="http://www.corephp.com/joomla-products/wordpress-for-joomla.html" title="WordPress for Joomla! by 'corePHP'">Joomla! extension</a> by <a href="http://www.corephp.com/" title="Joomla! extension">'corePHP'</a>
+		</div><!-- #site-generator -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-</body>
-</html>
--- wp30/src/components/com_wordpress/wp/wp-content/themes/twentytwelve/header.php
+++ wp30/src/components/com_wordpress/wp/wp-content/themes/twentytwelve/header.php
@@ -8,7 +8,8 @@
  * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
  */
-?><!DOCTYPE html>
+?>
+<?php /*<!DOCTYPE html>
 <!--[if IE 7]>
 <html class="ie ie7" <?php language_attributes(); ?>>
 <![endif]-->
@@ -18,20 +19,107 @@
 <!--[if !(IE 7) | !(IE 8)  ]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
-<head>
+<head>*/ ?>
+<?php
+global $mainframe;
+
+$document =& JFactory::getDocument();
+$menu = $mainframe->getMenu()->getActive();
+
+$title_override = '';
+
+if ( !empty( $menu ) ) {
+	if ( $menu->params->get( 'page_title' ) ) {
+		$title_override = $menu->params->get( 'page_title' );
+	}
+}
+
+// Add all of the head stuff to the head tag
+ob_start();
+?>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
+<title><?php
+	/*
+	 * Print the <title> tag based on what is being viewed.
+	 */
+	global $page, $paged;
+
+	wp_title( '|', true, 'right' );
+
+	// Add the blog name.
+	bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		echo " | $site_description";
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 )
+		echo ' | ' . sprintf( __( 'Page %s', 'twentytwelve' ), max( $paged, $page ) );
+
+	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
-<?php wp_head(); ?>
-</head>
+<?php
+	wp_head();
 
-<body <?php body_class(); ?>>
+$wp_head_contents = ob_get_contents();
+ob_end_clean();
+
+// Get the title for the page
+$title_start = '<title>';
+$title_end = '</title>';
+$_title = substr( $wp_head_contents,
+	strpos( $wp_head_contents, $title_start ) + 7,
+	strpos( $wp_head_contents, $title_end )
+		- strpos( $wp_head_contents, $title_start ) - 7);
+$_title = html_entity_decode( $_title, ENT_QUOTES, 'UTF-8' );
+
+// Title override
+if ( $title_override ) {
+	$_title = $title_override;
+}
+
+// Check to see if all in one is installed
+if ( class_exists('All_in_One_SEO_Pack') ) {
+	$_temp_title = $document->getTitle();
+	if ( !$_temp_title ) {
+		$document->setTitle( $_title );
+	} else {
+		$document->setTitle( str_replace( array( '&#8216;', '&#8217;', '&#8211;', '&#039;' ),
+			array( '\'', '\'', ' ', "'" ), $_temp_title ) );
+	}
+} else {
+	$document->setTitle( $_title );
+}
+
+$wp_head_contents = str_replace( $title_start . $_title . $title_end, '', $wp_head_contents );
+
+$document->addCustomTag( $wp_head_contents );
+unset( $wp_head_contents );
+
+//Add pathway a.k.a. breadcrumbs
+$pathway	= &$mainframe->getPathway();
+if(!is_home()){
+	/* More specific * /
+	if(is_tag())
+		$pathway->addItem(__('Tag').': '.wp_title('', false, 'right'), '');
+	elseif(is_category())
+		$pathway->addItem(__('Category').': '.wp_title('', false, 'right'), '');
+	elseif(is_date())
+		$pathway->addItem(__('Archive for ').wp_title('', false, 'right'), '');
+	else
+	/* */
+		$pathway->addItem(wp_title('', false, 'right'), '');
+}
+?>
+
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
