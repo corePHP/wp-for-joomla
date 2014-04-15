e8fbb4b860e3daff2c4ae2fcc455bd466729f2f1
collin <collin@corephp.com>
2014-04-15 09:05:47 -0400

    Make "use sidebar in content area" option work with TwentyTwelve & Make TwentyTwelve 100% width if no sidebar

--- wp38/src/components/com_wordpress/wp/wp-content/themes/twentytwelve/sidebar.php
+++ wp38/src/components/com_wordpress/wp/wp-content/themes/twentytwelve/sidebar.php
@@ -8,6 +8,12 @@
  * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
  */
+
+/* rc_corephp */
+// If we don't want the sidebar to show
+if ( !get_site_option( 'wpj_use_wp_sidebar', 1 ) ) {
+	return;
+}
 ?>
 
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
